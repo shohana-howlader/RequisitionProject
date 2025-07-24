da9e3f5c1996ee2ff7bceab8f21becf3feae98da
shohana <166001432+shohana-howlader@users.noreply.github.com>
2025-07-24 17:53:57 +0600

    database

---
+++ RequisitionProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RequisitionProject.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Requisition> Requisitions { get; set; }
+        public DbSet<RequisitionItem> RequisitionItems { get; set; }
+        public DbSet<RequisitionApproval> RequisitionApprovals { get; set; }
+    }
+}
